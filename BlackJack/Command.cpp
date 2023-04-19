01f5379c24ccaabc043fd62b95d1c3aaad5b04ed
Cocaine <kokotkin.alexander@gmail.com>
2023-04-19 17:46:54 +0300

    Add more comand realizations

--- BlackJack/Command.cpp
+++ BlackJack/Command.cpp
@@ -1,1 +1,2 @@
 #include "Command.h"
+#include "Menu.h"
--- BlackJack/Command.h
+++ BlackJack/Command.h
@@ -1,4 +1,10 @@
 #pragma once
+#include "Menu.h"
+#include "GameMode.h"
+
+/**
+ * @brief 
+*/
 class Command
 {
 public:
@@ -6,9 +12,50 @@ public:
     virtual void Execute() = 0;
 };
 
-class CallMainMenuCommand : public Command
+/**
+ * @brief 
+*/
+class ShowPauseMenuCommand : public Command
 {
 public:
-    virtual void Execute() { CallMenu(); }
-    void CallMenu();
+    virtual void Execute() { ShowPauseMenu(); }
+
+    void ShowPauseMenu() 
+    { 
+        Menu::GetInstance().ShowPauseMenu();
+    }
+};
+
+/**
+ * @brief *
+*/
+class ShowExitMenuCommand : public Command
+{
+public:
+    virtual void Execute() { ShowExitMenu(); }
+
+    void ShowExitMenu() 
+    {
+        Menu::GetInstance().ShowExitMenu();
+    }
+};
+
+/**
+ * @brief 
+*/
+class ShowRulesCommand : public Command
+{
+public:
+    virtual void Execute() { ShowRules(); }
+
+    void ShowRules()
+    {
+        Menu::GetInstance().ShowRules();
+
+        if (GameMode::GetInstance().GetGameState() == GameState::Pause) {
+            Menu::GetInstance().ShowPauseMenu();
+        }
+        else Menu::GetInstance().ShowMainMenu();
+
+    }
 };
