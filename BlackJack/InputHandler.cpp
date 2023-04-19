4573e945cd110089e4e37df93f2bdf25241de85c
Cocaine <kokotkin.alexander@gmail.com>
2023-04-19 17:46:32 +0300

    Add new commands and buttons

--- BlackJack/InputHandler.cpp
+++ BlackJack/InputHandler.cpp
@@ -1,1 +1,29 @@
 #include "InputHandler.h"
+#include "Command.h"
+
+InputHandler::~InputHandler()
+{
+    delete button_ESC;
+}
+
+void InputHandler::HandleInput(Button button)
+{
+    switch (button)
+    {
+    case Button::Button_ESC: button_ESC->Execute(); break;
+    case Button::Button_0: button_0->Execute(); break;
+    case Button::Button_1: button_0->Execute(); break;
+    case Button::Button_2: button_2->Execute(); break;
+    case Button::Button_3: button_3->Execute(); break;
+    default:
+        break;
+    }
+}
+
+InputHandler::InputHandler()
+{
+    button_ESC = new ShowPauseMenuCommand();
+    button_2 = new ShowRulesCommand();
+    button_3 = new ShowExitMenuCommand();
+
+}
--- BlackJack/InputHandler.h
+++ BlackJack/InputHandler.h
@@ -1,4 +1,48 @@
 #pragma once
+
+class Command;
+
+enum class Button {
+    Button_ESC = 27,
+    Button_0 = 48,
+    Button_1 = 49,
+    Button_2 = 50,
+    Button_3 = 51,
+    Button_H = 72,
+    Button_S = 83,
+    Button_D = 68,
+    Button_B = 66,
+    Button_Y = 89,
+    Button_N = 78
+};
+
 class InputHandler
 {
+public:
+    ~InputHandler();
+    static InputHandler& GetInstance() { static InputHandler instance; return instance; };
+    void HandleInput(Button button);
+
+    bool IsBlocked() const { return isBlocked; }
+    void Block(bool value) { isBlocked = value; }
+
+private:
+    bool isBlocked = false;
+
+    Command* button_ESC;
+    Command* button_0;
+    Command* button_1;
+    Command* button_2;
+    Command* button_3;
+    Command* button_H;
+    Command* button_S;
+    Command* button_D;
+    Command* button_B;
+    Command* button_Y;
+    Command* button_N;
+
+    InputHandler();
+    InputHandler(const InputHandler&) = delete;
+    InputHandler& operator = (const InputHandler&) = delete;
+
 };
