cc5c43fbea71dcbcb4df5896bbf8392355b4f8a2
Cocaine <kokotkin.alexander@gmail.com>
2023-04-19 17:48:28 +0300

    Incapsulate UI to Menu class

--- BlackJack/Menu.cpp
+++ BlackJack/Menu.cpp
@@ -1,76 +1,57 @@
 #include "Menu.h"
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <conio.h>
 
 #define RULES_PATH "rules.txt"
 
-void Menu::StartGame()
+void Menu::ShowMainMenu()
 {
+    std::cout << "MAIN MENU:\t" << "1 - New Game\t" << "2 - Rules\t" << "3 - Exit" << std::endl;
+    std::cout << "Choose option to continue..." << std::endl;
 }
 
-void Menu::ShowRules()
+void Menu::ShowPauseMenu()
 {
-    system("cls");
-	std::ifstream fin(RULES_PATH);
-
-	if (!fin.is_open()) {
-		throw std::runtime_error("Error opening file;");
-	}
-
-	try
-	{
-		char ch;
-		while (fin.get(ch))
-		{
-			std::cout << ch;
-		}
-	}
-
-	catch (const std::exception& ex)
-	{
-		std::cerr << "Exception caught: " << ex.what() << std::endl;
-	}
-
-	fin.close();
-
-	std::cout << std::endl;
-
-	(isPaused) ? ShowPauseMenu() : ShowMainMenu();
-
+    std::cout << "PAUSE MENU:\t" << "0 - Continue\t" << "1 - New Game\t" << "2 - Rules\t" << "3 - Exit" << std::endl;
+    std::cout << "Choose option to continue..." << std::endl;
 }
 
-void Menu::ExitGame()
+void Menu::ShowGameMenu()
+{
+    std::cout << "GAME MENU:\t" << "H - Hit\t" << "1New Game\t" << "2. Rules\t" << "3. Exit" << std::endl;
+    std::cout << "Choose option to continue..." << std::endl;
+}
+
+void Menu::ShowExitMenu()
 {
 	std::cout << "Are you sure you want to exit the game?\tPress: y - yes / n - no" << std::endl;
-	char result;
-	std::cin >> result;
-	
-	if (result == 'y') exit(0);
 }
 
-void Menu::GoToMenu(MenuItem item)
-{
-	switch (item)
-	{
-	case MenuItem::NewGame: StartGame(); break;
-	case MenuItem::Rules: ShowRules(); break;
-	case MenuItem::Exit: ExitGame(); break;
-	default: std::cout << "Menu item doesn't exist."; break;
-	}
-}
-
- void Menu::ShowMainMenu()
+ void Menu::ShowRules()
  {
-	 isPaused = false;
+     system("cls");
+     std::ifstream fin(RULES_PATH);
 
-	 std::cout << "MAIN MENU:\t" << "1. New Game\t" << "2. Rules\t" << "3. Exit" << std::endl;
-	 std::cout << "Press button to continue..." << std::endl;
- }
+     if (!fin.is_open()) {
+         throw std::runtime_error("Error opening file;");
+     }
 
- void Menu::ShowPauseMenu()
- {
-	 isPaused = true;
+     try
+     {
+         char ch;
+         while (fin.get(ch))
+         {
+             std::cout << ch;
+         }
+     }
+
+     catch (const std::exception& ex)
+     {
+         std::cerr << "Exception caught: " << ex.what() << std::endl;
+     }
+
+     fin.close();
+
+     std::cout << std::endl;
  }
--- BlackJack/Menu.h
+++ BlackJack/Menu.h
@@ -1,30 +1,19 @@
 #pragma once
 
-enum class MenuItem {
-	NewGame = 49,
-	Rules = 50,
-	Exit = 51
-};
-
 class Menu
 {
-private:
-	bool isPaused = false;
-
 public:
 	static Menu& GetInstance() { static Menu instance; return instance; };
 
-	void GoToMenu(MenuItem item);
 	void ShowMainMenu();
 	void ShowPauseMenu();
+    void ShowGameMenu();
+    void ShowExitMenu();
 
+    void ShowRules();
 private:
 	Menu() {};
 	Menu(const Menu&) = delete;
 	Menu& operator = (const Menu&) = delete;
-
-	void StartGame();
-	void ShowRules();
-	void ExitGame();
 };
 
